abfd16688acf13d2eb55afc9a0c71bf0b0dcde04
seungeyonam <167507747+seungeyonam@users.noreply.github.com>
2024-04-20 01:25:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,3 @@ function startGame() {
   // Hide the "Game Start" button
   document.getElementById('start-button').style.display = 'none';
 }
-
-// Event listener for "Game Start" button click
-document.getElementById('start-button').addEventListener('click', startGame);
-
-// Function to handle Option 1 button click
-function chooseOption1() {
-  // Redirect the user to the page for Option 1
-  window.location.href = "option1.html";
-}
-
-// Function to handle Option 2 button click
-function chooseOption2() {
-  // Redirect the user to the page for Option 2
-  window.location.href = "option2.html";
-}
-
-// Event listener for Option 1 button click
-document.getElementById('option1').addEventListener('click', chooseOption1);
-
-// Event listener for Option 2 button click
-document.getElementById('option2').addEventListener('click', chooseOption2);
