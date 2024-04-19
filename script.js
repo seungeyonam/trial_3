9047c7f014dd897eaa9dbafc05cbf69b2545bd3a
seungeyonam <167507747+seungeyonam@users.noreply.github.com>
2024-04-20 01:26:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ function startGame() {
   // Hide the "Game Start" button
   document.getElementById('start-button').style.display = 'none';
 }
+
+// Event listener for "Game Start" button click
+document.getElementById('start-button').addEventListener('click', startGame);
