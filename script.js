fcd43877eee2454fb25210b212ec44123ae5270f
seungeyonam <167507747+seungeyonam@users.noreply.github.com>
2024-04-20 01:31:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function startGame() {
 
 // Event listener for "Game Start" button click
 document.getElementById('start-button').addEventListener('click', startGame);
+
+// Function to handle the "Start Game" button click
+function startGame() {
+  // Redirect the user to the next page
+  window.location.href = "next_page.html";
+}
+
+// Event listener for "Start Game" button click
+document.getElementById('start-button').addEventListener('click', startGame);
